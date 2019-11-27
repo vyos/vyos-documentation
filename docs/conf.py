8da3e3415c3817d9f68d0653b0d839610b0d8110
Christian Poessinger <christian@poessinger.com>
2019-11-27 17:35:42 +0100

    conf: set full version - 1.3.x (equuleus)

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = u'VyOS maintainers and contributors'
 version = u'1.3'
 
 # The full version, including alpha/beta/rc tags
-release = u'equuleus'
+release = u'1.3.x (equuleus)'
 
 # -- General configuration ---------------------------------------------------
 
