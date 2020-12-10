b71a670b91f3ff1273239b0a69b8511754d0e4e0
rebortg <github@ghlr.de>
2020-12-10 08:19:03 +0100

    linter: fix vyos-linter.py to run without args

--- .github/vyos-linter.py
+++ .github/vyos-linter.py
@@ -147,8 +147,7 @@ def main():
                     if handle_file_action(file) is False:
                         bool_error = False
     except Exception as e:
-        print(e)    
-        for root, dirs, files in os.walk("../docs"):
+        for root, dirs, files in os.walk("docs"):
             path = root.split(os.sep)
             for file in files:
                 if file[-4:] == ".rst":
