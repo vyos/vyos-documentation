261867066022aa775a8480839a0ae0ecd26778a3
Christian Poessinger <christian@poessinger.com>
2021-01-24 19:50:58 +0100

    Bump version to 1.4

--- docs/conf.py
+++ docs/conf.py
@@ -26,10 +26,10 @@ copyright = u'2021, VyOS maintainers and contributors'
 author = u'VyOS maintainers and contributors'
 
 # The short X.Y version
-version = u'1.3'
+version = u'1.4'
 
 # The full version, including alpha/beta/rc tags
-release = u'1.3.x (equuleus)'
+release = u'1.4.x (sagitta)'
 
 # -- General configuration ---------------------------------------------------
 
