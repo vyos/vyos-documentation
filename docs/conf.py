e59f1ef3fffe90e66e98af6638bd8cc6f599657e
rebortg <github@ghlr.de>
2022-10-21 12:43:12 +0200

    prepare transifex

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,12 @@ master_doc = 'index'
 # Usually you set "language" from the command line for these cases.
 language = None
 
+# https://docs.readthedocs.io/en/stable/guides/manage-translations-sphinx.html#create-translatable-files
+locale_dirs = ['_locale/']
+gettext_compact = False
+gettext_uuid = True
+
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
